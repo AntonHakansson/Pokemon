6dd016363a1a6013027dc24e60da5a05671dd54d
Anton Håkansson <anton.hakansson98@gmail.com>
2016-08-17 14:14:06 +0200

    Added sorting by base xp

--- src/components/PokedexViewer.js
+++ src/components/PokedexViewer.js
@@ -22,7 +22,8 @@ injectTapEventPlugin();
 
 const idSortIcon = {index: 0, div: <FontIcon className="material-icons">history</FontIcon>};
 const alphaSortIcon = {index: 1, div: <FontIcon className="material-icons">sort_by_alpha</FontIcon>};
-const favoritesIcon = {index: 2, div: <FontIcon className="material-icons">favorite</FontIcon>};
+const xpSortIcon = {index: 2, div: <FontIcon className="material-icons">trending_up</FontIcon>};
+const favoritesIcon = {index: 3, div: <FontIcon className="material-icons">favorite</FontIcon>};
 
 class PokedexViewer extends React.Component {
   constructor(props)
@@ -42,7 +43,7 @@ class PokedexViewer extends React.Component {
     let newPokemonList =  Array(20).fill(undefined); // Fake loading
     this.forceUpdate();
     Pokedex.getPokemonsList().then(ress => {
-      let pokemons = ress.results.slice(0, 10);
+      let pokemons = ress.results.slice(0, 15);
       newPokemonList = Array(pokemons.length).fill(undefined);
       this.setState({pokemonList: this.state.pokemonList});
       return pokemons;
@@ -67,6 +68,12 @@ class PokedexViewer extends React.Component {
         return a.id-b.id;
       });
     }
+    if(this.state.selectedFilter == xpSortIcon.index)
+    {
+      this.state.pokemonList.sort(function (a, b) {
+        return b.base_experience-a.base_experience;
+      });
+    }
     if(this.state.selectedFilter == alphaSortIcon.index)
     {
       this.state.pokemonList.sort(function (a, b) {
@@ -86,7 +93,7 @@ class PokedexViewer extends React.Component {
         return(
           <GridTile key={pokemon.name}
                      title={pokemon.name}
-                      subtitle={<span>{pokemon.base_experience} CP<br/>{pokemon.types.map(type => {return type.type.name + " ";})}</span>}
+                      subtitle={<span>{pokemon.base_experience} Base XP<br/>{pokemon.types.map(type => {return type.type.name + " ";})}</span>}
                        actionIcon={<FontIcon style={{cursor: 'pointer', marginRight: 20}} className="material-icons" onClick={() => {pokemon.dialogOpen = true; this.forceUpdate();}}>info</FontIcon>}
                        >
             <img src={pokemon.sprites.front_default}></img>
@@ -95,7 +102,7 @@ class PokedexViewer extends React.Component {
               modal={false}
               open={pokemon.dialogOpen}
               onRequestClose={() => {pokemon.dialogOpen = false; this.forceUpdate();}} >
-                <span>Base CP: {pokemon.base_experience}</span>
+                <span>Base XP: {pokemon.base_experience}</span>
                 {dialogDivider}
                 <span>Base type: {pokemon.types.map(type => {return type.type.name + " ";})}</span>
                 {dialogDivider}
@@ -141,12 +148,17 @@ class PokedexViewer extends React.Component {
             onTouchTap={() => this.selectFilter(alphaSortIcon.index)}
           />
           <BottomNavigationItem
+            label="XP"
+            icon={xpSortIcon.div}
+            onTouchTap={() => this.selectFilter(xpSortIcon.index)}
+          />
+          <BottomNavigationItem
             label="Favorites"
             icon={favoritesIcon.div}
             onTouchTap={() => this.selectFilter(favoritesIcon.index)}
           />
         </BottomNavigation>
-        <GridList cellHeight={200} style={styles.gridList}>
+        <GridList cols={3} style={styles.gridList}>
           {this.getPokemonDOM()}
         </GridList>
         </div>
